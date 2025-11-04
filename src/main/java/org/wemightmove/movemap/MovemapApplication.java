dd9620ea23434933480912056b51c6f59ad9c136
crhwang822 <crhwang822@gmail.com>
2025-11-04 16:04:18 +0900

    Initial commit

---
+++ src/main/java/org/wemightmove/movemap/MovemapApplication.java
@@ -0,0 +1,13 @@
+package org.wemightmove.movemap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovemapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovemapApplication.class, args);
+    }
+
+}
