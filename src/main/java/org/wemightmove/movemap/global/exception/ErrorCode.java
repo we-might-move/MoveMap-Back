cba2298a662fad21dc489156121931b7a809ecbc
crhwang822 <crhwang822@gmail.com>
2025-11-08 23:28:18 +0900

    Chore: 에러 코드 오타 수정

--- src/main/java/org/wemightmove/movemap/global/exception/ErrorCode.java
+++ src/main/java/org/wemightmove/movemap/global/exception/ErrorCode.java
@@ -17,7 +17,7 @@ public enum ErrorCode {
     INVALID_FILE_FORMAT(400, 1500, "잘못된 파일 형식입니다."),
     UNSUPPORTED_FILE_FORMAT(400, 1501, "지원하지 않는 파일 형식입니다."),
     FILE_UPLOAD_FAIL(500, 1502, "파일 업로드에 실패했습니다."),
-    FILE_COMPARISON_FAIL(500, 1502, "파일 비교에 실패했습니다."),
+    FILE_COMPARISON_FAIL(500, 1503, "파일 비교에 실패했습니다."),
 
     /*
      * 인증/인가 관련 오류
