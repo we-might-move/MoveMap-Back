591f4b77e94cb2a04f4e3be7d1f86a4a403f70cf
crhwang822 <crhwang822@gmail.com>
2025-11-13 17:39:54 +0900

    Chore: Swagger에 JWT 인증 버튼 셋업

--- src/main/java/org/wemightmove/movemap/global/config/SwaggerConfig.java
+++ src/main/java/org/wemightmove/movemap/global/config/SwaggerConfig.java
@@ -2,8 +2,17 @@ package org.wemightmove.movemap.global.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+
+import java.util.Arrays;
 
 @OpenAPIDefinition(
         info = @Info(title = "MoveMap API Docs", version = "v1")
@@ -12,31 +21,31 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
-//    @Bean
-//    public OpenAPI openAPI() {
-//        SecurityScheme bearerAuth = new SecurityScheme()
-//                .type(SecurityScheme.Type.HTTP)
-//                .scheme("bearer")
-//                .bearerFormat("JWT")
-//                .in(SecurityScheme.In.HEADER)
-//                .name(HttpHeaders.AUTHORIZATION);
-//
-//        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
-//
-//        return new OpenAPI()
-//                .components(new Components()
-//                        .addSecuritySchemes("bearerAuth", bearerAuth))
-//                .security(Arrays.asList(securityRequirement));
-//    }
-//
-//
-//    @Bean
-//    public GroupedOpenApi chatOpenApi() {
-//        String[] paths = {"/**"};
-//
-//        return GroupedOpenApi.builder()
-//                .group("we-might-move")
-//                .pathsToMatch(paths)
-//                .build();
-//    }
+    @Bean
+    public OpenAPI openAPI() {
+        SecurityScheme bearerAuth = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name(HttpHeaders.AUTHORIZATION);
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", bearerAuth))
+                .security(Arrays.asList(securityRequirement));
+    }
+
+
+    @Bean
+    public GroupedOpenApi chatOpenApi() {
+        String[] paths = {"/**"};
+
+        return GroupedOpenApi.builder()
+                .group("we-might-move")
+                .pathsToMatch(paths)
+                .build();
+    }
 }
