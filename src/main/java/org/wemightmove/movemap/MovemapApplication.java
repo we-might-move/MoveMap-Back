44b85080b97d9f16c49c3dc620581dc74b38e999
crhwang822 <crhwang822@gmail.com>
2025-11-17 07:15:22 +0900

    Chore: Cors 설정 추가

--- src/main/java/org/wemightmove/movemap/MovemapApplication.java
+++ src/main/java/org/wemightmove/movemap/MovemapApplication.java
@@ -2,10 +2,13 @@ package org.wemightmove.movemap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.wemightmove.movemap.global.config.CorsConfigProperties;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableConfigurationProperties(CorsConfigProperties.class)
 public class MovemapApplication {
 
     public static void main(String[] args) {
