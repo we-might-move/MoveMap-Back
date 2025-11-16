4ce3ba20575a441f72d4da9133e72b47945d129f
5nam <ohnam90@gmail.com>
2025-11-16 17:08:35 +0900

    Refactor: 초대 목록 조회 API 엔드포인트 분리 단일 엔드포인트에서 type 파라미터로 분기 처리하던 방식을 별도 엔드포인트로 분리하여 명확성과 유지보수성 개선 변경사항: GET /members/invitations?type=sent -> GET /members/invitations/sent, GET /members/invitations?type=received -> GET /members/invitations/received

--- src/main/java/org/wemightmove/movemap/domain/member/controller/MemberController.java
+++ src/main/java/org/wemightmove/movemap/domain/member/controller/MemberController.java
@@ -1,5 +1,7 @@
 package org.wemightmove.movemap.domain.member.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -7,12 +9,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.wemightmove.movemap.domain.member.dto.response.ReceivedInviteResponse;
+import org.wemightmove.movemap.domain.member.dto.response.SentInviteResponse;
 import org.wemightmove.movemap.domain.member.service.MemberQueryService;
 
 @RestController
-@Tag(name = "Member")
 @RequiredArgsConstructor
 @RequestMapping("/members")
+@Tag(name = "회원", description = "회원 관리 API")
 public class MemberController {
 
     private final MemberQueryService memberQueryService;
@@ -20,9 +24,22 @@ public class MemberController {
     /**
      * FIXME : memberId 쿼리로 받는 것 로그인 구현 완료 되면 수정
      */
-    @GetMapping("/invitations")
-    public ResponseEntity<?> getInviteInfoList(@RequestParam("type") String type, @RequestParam("memberId") Long memberId) {
-        return type.equals("sent") ? ResponseEntity.ok(memberQueryService.getSentInviteList(memberId))
-                : ResponseEntity.ok(memberQueryService.getReceivedInviteList(memberId));
+    @Operation(summary = "보낸 초대 목록 조회(부모 사용)")
+    @GetMapping("/invitations/sent")
+    public ResponseEntity<SentInviteResponse> getSentInvitations(
+            @RequestParam("memberId") Long memberId
+    ) {
+        return ResponseEntity.ok(memberQueryService.getSentInviteList(memberId));
+    }
+
+    /**
+     * FIXME : memberId 쿼리로 받는 것 로그인 구현 완료 되면 수정
+     */
+    @Operation(summary = "받은 초대 목록 조회(아이 사용)")
+    @GetMapping("/invitations/received")
+    public ResponseEntity<ReceivedInviteResponse> getReceivedInvitations(
+            @RequestParam("memberId") Long memberId
+    ) {
+        return ResponseEntity.ok(memberQueryService.getReceivedInviteList(memberId));
     }
 }
