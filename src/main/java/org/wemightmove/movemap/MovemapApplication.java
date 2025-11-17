799742d4d116979f57b96497d8db34328e0b9b87
crhwang822 <crhwang822@gmail.com>
2025-11-17 11:17:53 +0900

    Chore: Swagger 서버 URL 지정

--- src/main/java/org/wemightmove/movemap/MovemapApplication.java
+++ src/main/java/org/wemightmove/movemap/MovemapApplication.java
@@ -1,5 +1,7 @@
 package org.wemightmove.movemap;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -9,6 +11,12 @@ import org.wemightmove.movemap.global.config.CorsConfigProperties;
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableConfigurationProperties(CorsConfigProperties.class)
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "${springdoc.swagger-ui.production}", description = "Production Server"),
+                @Server(url = "${springdoc.swagger-ui.development}", description = "Local Development Server")
+        }
+)
 public class MovemapApplication {
 
     public static void main(String[] args) {
