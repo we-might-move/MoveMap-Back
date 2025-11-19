f21f7ebf36aa2331188a4153a33fb9952b56a27b
crhwang822 <crhwang822@gmail.com>
2025-11-19 17:45:35 +0900

    Feat: 카카오 로그인 기능 구현

---
+++ src/main/java/org/wemightmove/movemap/global/client/KakaoClient.java
@@ -0,0 +1,58 @@
+package org.wemightmove.movemap.global.client;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClient;
+import org.wemightmove.movemap.global.client.dto.KakaoProfileResponse;
+import org.wemightmove.movemap.global.client.dto.KakaoTokenResponse;
+
+@Component
+@RequiredArgsConstructor
+public class KakaoClient {
+
+    private final RestClient restClient = RestClient.builder().build();
+
+    @Value("${kakao.client-id}")
+    private String clientId;
+
+    @Value("${kakao.redirect-uri}")
+    private String redirectUri;
+
+    @Value("${kakao.token-uri}")
+    private String tokenUri;
+
+    @Value("${kakao.user-info-uri}")
+    private String userInfoUri;
+
+
+    // 인가 코드로 토큰 요청
+    public KakaoTokenResponse getAccessToken(String code) {
+        MultiValueMap<String, String> form = new LinkedMultiValueMap<>();
+        form.add("grant_type", "authorization_code");
+        form.add("client_id", clientId);
+        form.add("redirect_uri", redirectUri);
+        form.add("code", code);
+
+        return restClient.post()
+                .uri(tokenUri)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(form)
+                .retrieve()
+                .body(KakaoTokenResponse.class);
+    }
+
+
+    // 액세스 토큰으로 사용자 정보 요청
+    public KakaoProfileResponse getUserInfo(String accessToken) {
+        return restClient.post()
+                .uri(userInfoUri)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                .retrieve()
+                .body(KakaoProfileResponse.class);
+    }
+}
