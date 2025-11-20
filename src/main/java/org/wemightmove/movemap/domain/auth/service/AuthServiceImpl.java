cd667a7630843ee9e85582669d7260f2a68841af
crhwang822 <crhwang822@gmail.com>
2025-11-20 09:34:31 +0900

    Chore: 디버깅 코드 삭제

--- src/main/java/org/wemightmove/movemap/domain/auth/service/AuthServiceImpl.java
+++ src/main/java/org/wemightmove/movemap/domain/auth/service/AuthServiceImpl.java
@@ -81,8 +81,6 @@ public class AuthServiceImpl implements AuthService{
         KakaoProfileResponse kakaoProfileResponse = kakaoClient.getUserInfo(kakaoTokenResponse.accessToken());
         Member member = memberRepository.findByKakaoId(kakaoProfileResponse.id()).orElse(null);
 
-        System.out.println(kakaoProfileResponse.id());
-
         if(member == null) {
             return KakaoLoginResponse.of(kakaoProfileResponse.id());
         }
