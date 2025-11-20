11942ac4fed63efae6d454358c3ecaad25f79c59
crhwang822 <crhwang822@gmail.com>
2025-11-20 09:48:32 +0900

    Feat: 외부 API 호출 예외 처리 구현

--- src/main/java/org/wemightmove/movemap/global/client/KakaoClient.java
+++ src/main/java/org/wemightmove/movemap/global/client/KakaoClient.java
@@ -20,9 +20,7 @@ import org.wemightmove.movemap.global.exception.ErrorCode;
 @RequiredArgsConstructor
 public class KakaoClient {
 
-    private final RestClient restClient = RestClient.builder().build();
     private final RestClient restClient = RestClient.builder()
-            .requestFactory(createRequestFactory())
             .defaultStatusHandler(status -> status.is4xxClientError() || status.is5xxServerError(),
                     (request, response) -> {
                         throw new CustomException(ErrorCode.EXTERNAL_API_ERROR);
