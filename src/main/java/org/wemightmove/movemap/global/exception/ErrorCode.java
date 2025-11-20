1a8feb16197627eb3f85cbd73e38f62e33e99ca0
crhwang822 <crhwang822@gmail.com>
2025-11-20 17:16:01 +0900

    Feat: 걷기 기록 동기화 기능 구현

--- src/main/java/org/wemightmove/movemap/global/exception/ErrorCode.java
+++ src/main/java/org/wemightmove/movemap/global/exception/ErrorCode.java
@@ -52,8 +52,9 @@ public enum ErrorCode {
      */
     FAIL_SERIALIZATION(500, 5001, "직렬화/역직렬화에 실패했습니다."),
     INVITE_EXPIRED(401, 5002, "만료된 초대입니다."),
-    INVITE_NOT_FOUND(HttpStatus.NOT_FOUND.value(), 5003, "초대 정보를 찾을 수 없습니다.");
-
+    INVITE_NOT_FOUND(HttpStatus.NOT_FOUND.value(), 5003, "초대 정보를 찾을 수 없습니다."),
+    RESOURCE_NOT_FOUND(404, 5000, "리소스를 찾을 수 없습니다.")
+    ;
 
     private final int status;
     private final int code;
