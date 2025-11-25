edd234284d23e18f1621223870f8a34aa6784f82
5nam <ohnam90@gmail.com>
2025-11-25 12:20:17 +0900

    Fix: RedisConfig 빈, CorsConfigProperties 빈 2개 생겨 충돌나는 오류 해결

--- src/main/java/org/wemightmove/movemap/global/config/RedisConfig.java
+++ src/main/java/org/wemightmove/movemap/global/config/RedisConfig.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -13,6 +14,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     @Bean
+    @Primary
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
