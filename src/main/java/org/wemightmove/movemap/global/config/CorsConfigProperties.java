b8294310cda691388fe79356b9fc3ba6b0c59d20
5nam <ohnam90@gmail.com>
2025-11-26 10:48:31 +0900

    Fix: SecurityConfig required a single bean, but 2 오류 CorsConfigProperties에서 @Configuration 지워서 해결

--- src/main/java/org/wemightmove/movemap/global/config/CorsConfigProperties.java
+++ src/main/java/org/wemightmove/movemap/global/config/CorsConfigProperties.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
-@Configuration
 @ConfigurationProperties(prefix = "cors")
 @Getter @Setter
 public class CorsConfigProperties {
