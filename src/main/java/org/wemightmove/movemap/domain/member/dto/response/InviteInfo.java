58ea3690e059272804a9e35d8869709d6566669a
5nam <ohnam90@gmail.com>
2025-12-04 09:35:26 +0900

    Fix: 초대 목록 조회 시 사용자 닉네임 커럼 추가

--- src/main/java/org/wemightmove/movemap/domain/member/dto/response/InviteInfo.java
+++ src/main/java/org/wemightmove/movemap/domain/member/dto/response/InviteInfo.java
@@ -6,7 +6,9 @@ import java.time.LocalDateTime;
 
 public record InviteInfo(
         Long parentId,
+        String parentName,
         Long childId,
+        String childName,
         @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
         LocalDateTime invitedAt
 ) {
@@ -14,7 +16,7 @@ public record InviteInfo(
     /**
      * TODO: 이 부분 LocalDateTime.now() 로 사용하는게 아니라 외부 LocalDateTime 유틸 만들어서 사용하기(테스트 코드 작성 시 의존성 문제 해결)
      */
-    public static InviteInfo of(Long parentId, Long childId, LocalDateTime localDateTime) {
-        return new InviteInfo(parentId, childId, localDateTime);
+    public static InviteInfo of(Long parentId, String parentName, Long childId, String childName, LocalDateTime localDateTime) {
+        return new InviteInfo(parentId, parentName, childId, childName, localDateTime);
     }
 }
