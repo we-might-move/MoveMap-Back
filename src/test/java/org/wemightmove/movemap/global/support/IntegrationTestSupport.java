452ca1261300a9aea386528c18608a7574bacfff
5nam <ohnam90@gmail.com>
2025-12-22 15:08:17 +0900

    Test: Testcontainers 기반 통합 테스트 환경 구성

---
+++ src/test/java/org/wemightmove/movemap/global/support/IntegrationTestSupport.java
@@ -0,0 +1,25 @@
+package org.wemightmove.movemap.global.support;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+public abstract class IntegrationTestSupport {
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        // PostgreSQL
+        var postgres = TestContainers.getPostgres();
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+
+        // Redis
+        var redis = TestContainers.getRedis();
+        registry.add("spring.data.redis.host", redis::getHost);
+        registry.add("spring.data.redis.port", () -> redis.getMappedPort(6379));
+    }
+}
