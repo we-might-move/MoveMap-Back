eb9eab8e32e6350803ea1182c23f0015844a6d6b
5nam <ohnam90@gmail.com>
2026-01-04 20:56:16 +0900

    Fix: 전체 테스트 시 DB가 안띄어지는 오류와 자동으로 파라미터 이름이 설정되지 않는 오류 해결

--- src/test/java/org/wemightmove/movemap/MovemapApplicationTests.java
+++ src/test/java/org/wemightmove/movemap/MovemapApplicationTests.java
@@ -2,9 +2,10 @@ package org.wemightmove.movemap;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.wemightmove.movemap.global.support.IntegrationTestSupport;
 
 @SpringBootTest
-class MovemapApplicationTests {
+class MovemapApplicationTests extends IntegrationTestSupport {
 
     @Test
     void contextLoads() {
