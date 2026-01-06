c9c3d1455f712414063e2bac815c5903aff667d1
5nam <ohnam90@gmail.com>
2026-01-06 20:27:36 +0900

    Fix: 통합 테스트 클래스의 중복 @SpringBootTest 어노테이션 제거
    - MovemapApplicationTests에서 제거
    - 부모 클래스(IntegrationTestSupport)의 설정과 충돌하여 javaMailSender Bean 중복 등록 오류 발생
    - 자식 클래스는 상속만 받도록 수정

--- src/test/java/org/wemightmove/movemap/MovemapApplicationTests.java
+++ src/test/java/org/wemightmove/movemap/MovemapApplicationTests.java
@@ -1,10 +1,8 @@
 package org.wemightmove.movemap;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.wemightmove.movemap.global.support.IntegrationTestSupport;
 
-@SpringBootTest
 class MovemapApplicationTests extends IntegrationTestSupport {
 
     @Test
